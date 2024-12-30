eadef782005634e470e95d57920b03d18d229f5c
andreiburdean <andrei.burdean1@gmail.com>
2024-12-30 16:05:31 +0200

    tot la fond de urgente

--- ProiectPIU/app/src/main/java/com/example/proiectpiu_managementfinanciar/budget/EmergencyBudgetActivity.kt
+++ ProiectPIU/app/src/main/java/com/example/proiectpiu_managementfinanciar/budget/EmergencyBudgetActivity.kt
@@ -3,9 +3,16 @@ package com.example.proiectpiu_managementfinanciar.budget
 import android.content.Intent
 import android.graphics.Rect
 import android.os.Bundle
+import android.os.Handler
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.proiectpiu_managementfinanciar.R
 import com.example.proiectpiu_managementfinanciar.home_dashboard.ParentDashboardActivity
 
@@ -33,6 +40,61 @@ class EmergencyBudgetActivity : AppCompatActivity() {
             val intent = Intent(this, EmergencyBudgetActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
+        }
+
+        val infoIcon : ImageView = findViewById(R.id.info_icon)
+        val infoText : TextView = findViewById(R.id.info_text)
+        val infoTextLung : TextView = findViewById(R.id.info_text_lung)
+        val amountTextfield : EditText = findViewById(R.id.sectionAmount)
+        val saveButton : Button = findViewById(R.id.save_button)
+        val messageSection : LinearLayout = findViewById(R.id.whatever_message_section)
+
+        infoIcon.setOnClickListener {
+            if (infoText.visibility == View.VISIBLE){
+                infoText.visibility = View.GONE
+                infoTextLung.visibility = View.VISIBLE
+                amountTextfield.visibility = View.GONE
+                saveButton.visibility = View.GONE
+            }
+            else{
+                infoText.visibility = View.VISIBLE
+                infoTextLung.visibility = View.GONE
+                amountTextfield.visibility = View.VISIBLE
+                saveButton.visibility = View.VISIBLE
+            }
+        }
+
+        saveButton.setOnClickListener {
+            if (amountTextfield.getText().toString().isEmpty()){
+                messageSection.visibility = View.VISIBLE
+
+                val importantIcon : ImageView = findViewById(R.id.important_icon)
+                importantIcon.visibility = View.VISIBLE
+
+                val whatEverText : TextView = findViewById(R.id.whatever_text)
+                whatEverText.visibility = View.VISIBLE
+
+                Handler().postDelayed({
+                    messageSection.visibility = View.GONE
+                    importantIcon.visibility = View.GONE
+                    whatEverText.visibility = View.GONE
+                }, 2000)
+            }
+            else{
+                messageSection.visibility = View.VISIBLE
+
+                val bell : ImageView = findViewById(R.id.bell)
+                bell.visibility = View.VISIBLE
+
+                val whatEverText2 : TextView = findViewById(R.id.whatever_text2)
+                whatEverText2.visibility = View.VISIBLE
+
+                Handler().postDelayed({
+                    messageSection.visibility = View.GONE
+                    bell.visibility = View.GONE
+                    whatEverText2.visibility = View.GONE
+                }, 2000)
+            }
         }
 
         setKeyboardVisibilityListener()
